999c1a4622cc0aee72404ce8b018699bba194610
Phoenix <ouledameur.amjad@gmail.com>
2019-09-23 16:00:44 +0200

    Définition de gpu_im_seg qui gère le traitement avec CUDA

--- im_seg_qt/gpu_seg.cu
+++ im_seg_qt/gpu_seg.cu
@@ -21,5 +21,35 @@ void get_green(uchar *green_im, QRgb *rgb_im, int width, int height) {
 
 extern "C"
 void gpu_im_seg(uchar *gray_im, QRgb *rgb_im, int width, int height) {
+    uchar *green_im; // pointeur memoire cpu, pointera sur la composante verte de l'image rgb
+    uchar *dev_gray_im, *dev_green_im; // pointeur memoire gpu
+    int n_pixels = width*height;
+    int size = n_pixels*sizeof(uchar);
+
+    green_im = (uchar*) malloc(size);
+    get_green(green_im, rgb_im, width, height);
+
+    // Allocation memoire GPU
+    cudaMalloc((void**) &dev_green_im, size);
+    cudaMalloc((void**) &dev_gray_im, size);
+
+    cudaMemcpy(dev_green_im, green_im, size, cudaMemcpyHostToDevice);
+
+    // Recuperation des caractéristiques du gpu (nombre de threads par bloc)
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0);
+    int blockSize = deviceProp.maxThreadsPerBlock;
+    int numBlocks = (n_pixels - 1) / blockSize + 1;
+
+    // Appel du kernel cuda pour realiser la segmentation
+    seg_kernel<<<numBlocks, blockSize>>>(dev_gray_im, dev_green_im, n_pixels);
+    cudaDeviceSynchronize();
+
+    // Recuperation de l'image segmentee
+    cudaMemcpy(gray_im, dev_gray_im, size, cudaMemcpyDeviceToHost);
+
+    // Desallocation memoire
+    free(green_im);
+    cudaFree(dev_gray_im); cudaFree(dev_green_im);
 }
 
