b70dcd7de0aa69216934ca96988f1489c37064b0
Phoenix <ouledameur.amjad@gmail.com>
2019-09-23 15:49:35 +0200

    Définition de la méthode permettant la segmentation avec le cpu

--- im_seg_qt/player_interface.cpp
+++ im_seg_qt/player_interface.cpp
@@ -1,13 +1,13 @@
 #include "player_interface.h"
 
-Player_interface::Player_interface()
-{
-
+Player_interface::Player_interface() {
+    rgb_ptr  = NULL;
+    gray_ptr = NULL;
 }
 
-Player_interface::~Player_interface()
-{
-
+Player_interface::~Player_interface() {
+    if (gray_ptr != NULL)
+        free(gray_ptr);
 }
 
 void Player_interface::openFile(QString* name) {
@@ -31,7 +31,20 @@ void Player_interface::openFile(QString* name) {
 }
 
 void Player_interface::cpu_seg() {
+    // Recuperation des pointeurs de l'image d'entree et de sortie
+    int n_pixels = width*height;
+    rgb_ptr = (QRgb*) rgb_im.bits();
+    gray_ptr = (uchar*) malloc(width*height*sizeof(uchar));
 
+    QRgb rgb_comp;
+    uchar gray_comp;
+   for(int i=0; i<n_pixels; i++) {
+       rgb_comp = (QRgb) rgb_ptr[i];;
+       gray_comp = (GET_GREEN(rgb_comp) > GREEN_TRESH) ? WHITE : BLACK;
+       gray_ptr[i] = gray_comp;
+   }
+   // Creation de l'image binarise
+   gray_im = QImage(gray_ptr, width, height, width, QImage::Format::Format_Grayscale8);
 }
 
 void Player_interface::gpu_seg() {
