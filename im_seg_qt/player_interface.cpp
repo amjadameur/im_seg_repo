b6a4af3b16f443fa1e1f2456eb21a051aff87031
Phoenix <ouledameur.amjad@gmail.com>
2019-09-23 15:48:35 +0200

    Définition de open_file permettant d'ouvrir l'image rgb

--- im_seg_qt/player_interface.cpp
+++ im_seg_qt/player_interface.cpp
@@ -11,7 +11,23 @@ Player_interface::~Player_interface()
 }
 
 void Player_interface::openFile(QString* name) {
+    QString file_name;
+    if(name == NULL)
+        file_name = QFileDialog::getOpenFileName(this,
+            tr("image rgb"), "",
+            tr("fichiers images (*.jpg *.png *.gif);;All Files (*)"));
+    else
+        file_name = *name;
 
+    if(file_name.isEmpty()) exit(0);
+
+    // Lecture de l'image rgb
+    QImageReader reader(file_name);
+    reader.setAutoTransform(true);
+    rgb_im = reader.read();
+
+    width  = rgb_im.width();
+    height = rgb_im.height();
 }
 
 void Player_interface::cpu_seg() {
